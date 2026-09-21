763459e087f72121607208001234308ab0859e6f
wael <waelkatiti16@gmail.com>
2026-09-21 12:46:51 +0200

    Use process.env.PORT

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Importation de l'application express
 const app = require('./backend/app');
-app.listen(8000, () => {
-    console.log('Server running on port 8000');
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
